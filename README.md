60ad9be3890569880d3338055390a7ab09518be9
tmlbl <tim@shippable.com>
2014-10-02 15:13:54 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+503 sender
+==========
+
+This sends a 503 error as a response to any http request. That is literally all it does.
+
+To start: `npm start`
+
+To test: `curl localhost:5003`
+
---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+
+app.all('*', function (req, res) {
+  console.log('Sending a 503');
+  res.status(503).send('Service Unavailable');
+});
+
+app.listen(5003, function () {
+  console.log('Listening on port 5003');
+});
+
