82ed308ad0f919bcfdad358bba6ab204dd6fe69e
Tim <tmlbl@users.noreply.github.com>
2014-10-02 15:22:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 
 app.all('*', function (req, res) {
   console.log('Sending a 503');
-  res.status(503).send('Service Unavailable');
+  res.status(503).send('Service Unavailable\n');
 });
 
 app.listen(5003, function () {
